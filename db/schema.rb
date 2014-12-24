3ee88f9cc15030be9760ff518f4686526097aac5
thanh ngo <ngvt@live.com>
2014-12-24 00:03:50 -0500

    generated model for list

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141221234904) do
+ActiveRecord::Schema.define(version: 20141224045000) do
+
+  create_table "lists", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
