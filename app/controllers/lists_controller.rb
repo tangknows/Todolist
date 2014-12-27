1761786787f58e9f34225a6f9002a11db1278da7
thanh ngo <ngvt@live.com>
2014-12-27 14:32:24 -0500

    updated to allow edits

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,14 +1,45 @@
 class ListsController < ApplicationController
+  before_action :authenticate_user! # users must be signed in before any lists_controller method
+  
   def index
   end
 
   def show
-    @list = current_user.list
+    @list = List.find(params[:id])
   end
 
   def new
+    @list = List.new
+  end
+  
+  def create
+    @list = List.new(params.require(:list).permit(:title, :body))
+      if @list.save
+        flash[:notice] = "List was saved."
+        redirect_to @list
+      else
+        flash[:error] = "There was an error saving the post. Please try again."
+        render :new
+      end
+  
   end
 
   def edit
+    @list = List.find(params[:id])
   end
+
+def update
+  @list = List.find(params[:id])
+
+  #authorize missing
+  
+  if @list.update_attributes(params.require(:list).permit(:title, :body))
+    flash[:notice] = "List was updated."
+    redirect_to [@list]
+  else
+    flash[:error] = "There was an error saving the post. Please try again."
+    render :new
+  end
+end
+
 end
---
+++ db/migrate/20141224194924_add_id_to_list.rb
@@ -0,0 +1,5 @@
+class AddIdToList < ActiveRecord::Migration
+  def change
+    add_column :lists, :user_id, :integer
+  end
+end
